34774a58eb2d0cdeed9d859821394c462a48bb33
ayumii <april_angela2@hotmail.com>
2013-06-14 00:19:31 +0800

    last edited 12.19am 14June angela

--- nus_module_review/giftbook.py
+++ nus_module_review/giftbook.py
@@ -23,12 +23,12 @@ class MainPage(webapp2.RequestHandler):
         'logout': users.create_logout_url(self.request.host_url),
         } 
       template = jinja_environment.get_template('frontuser.html')
-      self.response.out.write('Hello, '+ template.render(template_values))
+      self.response.out.write(template.render(template_values))
     else:
       self.redirect('/')
 
 class Login(webapp2.RequestHandler):
-  def get(self):
+  def get(self): #similar to pulling existing data
     user = users.get_current_user()
     if user:  # signed in already
       template_values = {
@@ -37,7 +37,7 @@ class Login(webapp2.RequestHandler):
         'logout': users.create_logout_url(self.request.host_url),
         }
       template = jinja_environment.get_template('frontuser.html')
-      self.response.out.write('Hello, '+template.render(template_values))
+      self.response.out.write(template.render(template_values))
     else:
       self.redirect(users.create_login_url(federated_identity='https://openid.nus.edu.sg/'))
 
@@ -51,21 +51,21 @@ class Items(db.Model):
   """Models an item with item_link, image_link, description, and date."""
   item_link = db.StringProperty()
   image_link = db.StringProperty()
-  description = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
+  description = db.StringProperty(multiline=True) # pressing enter will not affect the result, descrip. allows /n  
+  date = db.DateTimeProperty(auto_now_add=True) # add automatically upon calling the constructor for the entity. and the date is in GMT+0
     
 class AddList(webapp2.RequestHandler):
   """ Add an item to the datastore """
-  def post(self):
+  def post(self): #similar to pushing data
   # Retrieve person
-    parent_key = db.Key.from_path('Persons', users.get_current_user().email())
-    person = db.get(parent_key)
+    parent_key = db.Key.from_path('Persons', users.get_current_user().email()) #person -class represented as a string
+    person = db.get(parent_key) #it will return the person object that is associated with the key called parent_key
     if person == None:
       newPerson = Persons(key_name=users.get_current_user().email())
-      newPerson.put()
+      newPerson.put() #push object to store it in the database similar to vector.push_back()
 
-    item = Items(parent=parent_key)
-    item.item_link = self.request.get('item_url')
+    item = Items(parent=parent_key) #items=constructor, parent=parent_key, item=child
+    item.item_link = self.request.get('item_url')  #note only under POST method then we can use self.request.get to retrieve info from user
     item.image_link = self.request.get('image_url')
     item.description = self.request.get('desc')
 
@@ -87,7 +87,7 @@ class WishList(webapp2.RequestHandler):
 
       template_values = {
         'user_mail': users.get_current_user().email(),
-        'logout': users.create_logout_url(self.request.host_url),
+        'logout': users.create_logout_url(self.request.host_url), #host_url : default/main page of the webpage
         'items': query,
         } 
 
@@ -107,7 +107,7 @@ class WishListtwo(webapp2.RequestHandler):
       parent_key = db.Key.from_path('Persons', users.get_current_user().email())
 
       query = db.GqlQuery("SELECT * FROM Items WHERE ANCESTOR IS :1 ORDER BY date DESC", parent_key)
-
+      #ancestor is :1 means the first parameter passed is parent_key
       template_values = {
         'user_mail': users.get_current_user().email(),
         'logout': users.create_logout_url(self.request.host_url),
