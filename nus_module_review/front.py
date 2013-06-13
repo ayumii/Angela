83dd29753816b4fbe543e026c4fabac7506222ee
ayumii <april_angela2@hotmail.com>
2013-06-13 17:26:22 +0800

    5.30pm 13jun angela

--- nus_module_review/front.py
+++ nus_module_review/front.py
@@ -3,13 +3,13 @@ import jinja2
 import os
 
 jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
+	loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/templates"))
 
 class MainPage(webapp2.RequestHandler):
-  """ Handler for the front page."""
-  def get(self):
-      template = jinja_environment.get_template('front.html')
-      self.response.out.write(template.render())
-      
-app = webapp2.WSGIApplication([('/', MainPage)],
-                              debug=True)
+	""" Handler for the front page."""
+	def get(self):
+		template = jinja_environment.get_template('front.html')
+		self.response.out.write(template.render())
+		
+		app = webapp2.WSGIApplication([('/', MainPage)],
+			debug=True)
